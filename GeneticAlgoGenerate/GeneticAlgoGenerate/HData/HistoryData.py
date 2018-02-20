f9fd70acb890ee0f183e1baf944dba980495bc6b
RandomKori <randomkori40@gmail.com>
2018-02-20 11:41:12 +0600

    v1

--- GeneticAlgoGenerate/GeneticAlgoGenerate/HData/HistoryData.py
+++ GeneticAlgoGenerate/GeneticAlgoGenerate/HData/HistoryData.py
@@ -4,6 +4,6 @@ import datetime
 # Чтение тиковой истории форекс в таблицу pfndas и возвращение таймсерий для Bid и Ask
 def ReadTickBidAsk(name):
     rz=pd.read_csv(name,sep=',')
-    Bid=pd.Series(rz['Bid'].values,rz['DateTime'])
-    Ask=pd.Series(rz['Ask'].values,rz['DateTime'])
+    Bid=pd.Series(rz['Bid'].values, pd.to_datetime(rz['DateTime']))
+    Ask=pd.Series(rz['Ask'].values,pd.to_datetime(rz['DateTime']))
     return Bid,Ask
