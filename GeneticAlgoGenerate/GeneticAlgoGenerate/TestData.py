696da11859132ecea0b0f05bf1259dd87cfb478d
RandomKori <randomkori40@gmail.com>
2018-02-27 10:41:59 +0600

    v1

--- GeneticAlgoGenerate/GeneticAlgoGenerate/TestData.py
+++ GeneticAlgoGenerate/GeneticAlgoGenerate/TestData.py
@@ -1,10 +1,43 @@
 import pandas as pd
 import HData.HistoryData as dt
+import Tester.Tester as ts
 import matplotlib.pyplot as plt
+import random as rnd
 
+print('Загрузка данных')
 f=dt.ReadTickBidAsk('./History/EURUSD.m_Ticks.csv')
 print(f.head())
 f.plot('DateTime','Bid')
+plt.show()
+print('Тестирование')
+st=ts.CTester()
+st.TickHistory=f[f.columns[:]].values.tolist()
+sl=50
+tp=70
+def syst():
+    global st
+    pz=0
+    if len(st.position)==0:
+        p=rnd.random()
+        if p>0.5:
+            pz=1
+        else:
+            pz=-1
+    else:
+        if st.posirion[2]==1:
+            if st.position[3]-sl*st.Point<=dat[len(dat)-1,1]:
+                pz=-1
+            if st.position[3]+tp*st.Point>=dat[len(dat)-1,1]:
+                pz=-1
+        else:
+            if st.position[3]+sl*st.Point>=dat[len(dat)-1,2]:
+                pz=-1
+            if st.position[3]-tp*st.Point<=dat[len(dat)-1,2]:
+                pz=-1
+    return 0.1,pz
+st.System=syst
+st.Test()
+plt.plot(st.ProfitHistory[0],st.ProfitHistory[1])
 plt.show()
 input("Нажмите ентер")
 
--- GeneticAlgoGenerate/GeneticAlgoGenerate/Tester/Tester.py
+++ GeneticAlgoGenerate/GeneticAlgoGenerate/Tester/Tester.py
@@ -1,56 +1,106 @@
 
-class Tester:
-    self.TickHistory=[]
-    self.TradeHistory=[]
-    self.System # Функция реализуящая систему вида lot,trade=f(ticks) где tick список тиков размером N, trade 0 - нет сделки 1 - покупка -1 - продажа
-    self.N=20 # Количество тиков передающихся в функцию реализующую систему
-    self.Deposit=1000
-    self.TickValue=1 # Цена тика на 1 лот
-    self.Profit
-    self.MaxDropdawn
-    self. TradeCount
-    self.WinPercent
+class CTester:
+    def __init__(self):
+        self.TickHistory=[]
+        self.TradeHistory=[]
+        self.ProfitHistory=[]
+        self.dat=[]
+        self.position=[]
+        self.System=None # Функция реализуящая систему вида lot,trade=f(ticks) где tick список тиков размером N, trade 0 - нет сделки 1 - покупка -1 - продажа
+        self.N=20 # Количество тиков передающихся в функцию реализующую систему
+        self.Deposit=1000
+        self.TickValue=1 # Цена тика на 1 лот
+        self.Point=0.00001
+        self.Profit=0
+        self.MaxDropdawn=0
+        self. TradeCount=0
+        self.Win=0
 
     def Test(self):
         self.TradeHistory=[]
+        self.ProfitHistory=[]
+        self.position=[]
+        self. TradeCount=0
+        self.Win=0
+        balance=self.Deposit
         i=self.N-1
         last=len(self.TickHistory)
-        position=[]
+        self.position=[]
         while i<last:
-            if self.TickHistory[i,1]==self.TickHistory[i-1,1] and i!=self.N-1:
+            if self.TickHistory[i][1]==self.TickHistory[i-1][1] and i!=self.N-1:
                 i=i+1
                 continue
-            lot,trade=self.System(self.TickHistory[i-self.N-1:i])
-            if len(pozition)==0:
+            if balance<=0:
+                break
+            self.dat=self.TickHistory[(i-self.N-1):(i+1)]
+            lot,trade=self.System()
+            if len(self.position)==0:
                 if trade==1:
-                    t=[self.TickHistory[i,0],lot,trade,self.TickHistoty[i,2]]
-                    position=t
+                    t=[self.TickHistory[i][0],lot,trade,self.TickHistoty[i][2]]
+                    self.position=t
                     self.TradeHistory.append(t)
                 if trade==-1:
-                     t=[self.TickHistory[i,0],lot,trade,self.TickHistoty[i,1]]
-                     position=t
+                     t=[self.TickHistory[i][0],lot,trade,self.TickHistoty[i][1]]
+                     self.position=t
                      self.TradeHistory.append(t)
             else:
                 if trade==1:
-                    t=[self.TickHistory[i,0],lot,trade,self.TickHistoty[i,2]]
+                    t=[self.TickHistory[i][0],lot,trade,self.TickHistoty[i][2]]
                     self.TradeHistory.append(t)
-                    if position[1]==t[1] and position[2]==-1:
-                        position=[]
-                    if position[2]==1:
-                        position=[self.TickHistory[i,0],position[1]+t[1],1,(position[3]*position[1]+t[3]*t[1])/(position[1]+t[1])]
-                    if position[2]==-1 and t[1]<position[1]:
-                        position[1]=position[1]-t[1]
-                    if position[2]==-1 and t[1]>position[1]:
-                        position=[self.TickHistory[i,0],t[1]-position[1],-1,self.TickHistory[i,1]]
+                    if self.position[1]==t[1] and self.position[2]==-1:
+                        prof=(self.position[3]-t[3])/self.Point*self.position[1]*self.TivkValue
+                        balance=balance+prof
+                        self.ProfitHistory.apprnd([t[0],balance])
+                        self.TradeCount=self.TradeCount+1
+                        if prof>0:
+                            self.Win=self.Win+1
+                        self.position=[]
+                    if self.position[2]==1:
+                        self.position=[self.TickHistory[i][0],self.position[1]+t[1],1,(self.position[3]*self.position[1]+t[3]*t[1])/(self.position[1]+t[1])]
+                    if self.position[2]==-1 and t[1]<self.position[1]:
+                        self.position[1]=position[1]-t[1]
+                        self.TradeCount=self.TradeCount+1
+                        prof=(self.position[3]-t[3])/self.Point*t[1]*self.TivkValue
+                        balance=balance+prof
+                        self.ProfitHistory.apprnd([t[0],balance])
+                        if prof>0:
+                            self.Win=self.Win+1
+                    if self.position[2]==-1 and t[1]>self.position[1]:
+                        self.TradeCount=self.TradeCount+1
+                        prof=(self.position[3]-t[3])/self.Point*self.position[1]*self.TivkValue
+                        balance=balance+prof
+                        self.ProfitHistory.apprnd([t[0],balance])
+                        self.position=[self.TickHistory[i][0],t[1]-position[1],-1,self.TickHistory[i,1]]
+                        if prof>0:
+                            self.Win=self.Win+1
                 if trade==-1:
-                    t=[self.TickHistory[i,0],lot,trade,self.TickHistoty[i,1]]
+                    t=[self.TickHistory[i][0],lot,trade,self.TickHistoty[i][1]]
                     self.TradeHistory.append(t)
-                    if position[1]==t[1] and position[2]==1:
-                        position=[]
-                    if position[2]==-1:
-                        position=[self.TickHistory[i,0],position[1]+t[1],-1,(position[3]*position[1]+t[3]*t[1])/(position[1]+t[1])]
-                    if position[2]==1 and t[1]<position[1]:
-                        position[1]=position[1]-t[1]
-                    if position[2]==1 and t[1]>position[1]:
-                        position=[self.TickHistoey[i,0],t[1]-position[1],1,self.TickHistory[i,2]]
+                    if self.position[1]==t[1] and self.position[2]==1:
+                        prof=(t[3]-self.position[3])/self.Point*self.position[1]*self.TivkValue
+                        balance=balance+prof
+                        self.ProfitHistory.apprnd([t[0],balance])
+                        self.TradeCount=self.TradeCount+1
+                        if prof>0:
+                            self.Win=self.Win+1
+                        self.position=[]
+                    if self.position[2]==-1:
+                        self.position=[self.TickHistory[i][0],self.position[1]+t[1],-1,(self.position[3]*self.position[1]+t[3]*t[1])/(self.position[1]+t[1])]
+                    if self.position[2]==1 and t[1]<self.position[1]:
+                        prof=(t[3]-self.position[3])/self.Point*t[1]*self.TivkValue
+                        balance=balance+prof
+                        self.ProfitHistory.apprnd([t[0],balance])
+                        self.TradeCount=self.TradeCount+1
+                        self.position[1]=self.position[1]-t[1]
+                        if prof>0:
+                            self.Win=self.Win+1
+                    if self.position[2]==1 and t[1]>self.position[1]:
+                        self.TradeCount=self.TradeCount+1
+                        prof=(t[3]-self.position[3])/self.Point*self.position[1]*self.TivkValue
+                        balance=balance+prof
+                        self.ProfitHistory.apprnd([t[0],balance])
+                        self.position=[self.TickHistoey[i][0],t[1]-self.position[1],1,self.TickHistory[i,2]]
+                        if prof>0:
+                            self.Win=self.Win+1
             i=i+1
+        self.Profit=balance-self.Deposit
