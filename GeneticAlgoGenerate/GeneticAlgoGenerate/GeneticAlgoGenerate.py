7a1ef56309bb2379dcf2da4f337c48a8763c14b4
RandomKori <randomkori40@gmail.com>
2018-02-28 10:10:16 +0600

    v1

--- GeneticAlgoGenerate/GeneticAlgoGenerate/GeneticAlgoGenerate.py
+++ GeneticAlgoGenerate/GeneticAlgoGenerate/GeneticAlgoGenerate.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import HData.HistoryData as dt
+import Tester.Tester as ts
 
+print("Загрузка данных")
+f=dt.ReadTickBidAsk('./History/EURUSD.m_Ticks.csv')
+st=ts.CTester()
+st.TickHistory=f[f.columns[:]].values.tolist()
+st.N=10
+print("Обучение ГА")
+
+input("Нажмите ввод")
